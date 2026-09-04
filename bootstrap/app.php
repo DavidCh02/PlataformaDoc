7ac3ea93e8bcc72ce82735d69c19ca1917fd0655
David Rivera <chingocriss703@gamil.com>
2026-09-04 17:16:17 -0500

    13vo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,14 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Confía en el proxy HTTPS de Railway para evitar ERR_TOO_MANY_REDIRECTS
-        $middleware->trustProxies(at: '*');
-
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
